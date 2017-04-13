c994f0439f839539db587040f707135cd20712cc
Skirmantas Januškas <skirmantas.januskas@gmail.com>
2017-04-13 17:05:03 +0300

    Started creating admin pages

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -16,4 +16,40 @@ class AdminController extends Controller
         ));
     }
 
+    /**
+     * @Route("/admin/filters")
+     */
+    public function filtersAction()
+    {
+        return $this->render('AppBundle:Admin:filters.html.twig', array(// ...
+        ));
+    }
+
+    /**
+     * @Route("/admin/stats/visitors")
+     */
+    public function visitorStatsAction()
+    {
+        return $this->render('AppBundle:Admin:visitors_stats.html.twig', array(// ...
+        ));
+    }
+
+    /**
+     * @Route("/admin/stats/crawler")
+     */
+    public function crawlerStatsAction()
+    {
+        return $this->render('AppBundle:Admin:crawler_stats.html.twig', array(// ...
+        ));
+    }
+
+    /**
+     * @Route("/admin/settings")
+     */
+    public function settingsAction()
+    {
+        return $this->render('AppBundle:Admin:settings.html.twig', array(// ...
+        ));
+    }
+
 }
