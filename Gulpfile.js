69715469c25804b3267ac1a58460b52c67f3b834
Skirmantas Januškas <skirmantas.januskas@gmail.com>
2017-04-14 22:32:50 +0300

    Separated admin scripts from main

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,49 +1,62 @@
 'use strict';
 
-var gulp    = require('gulp');
-var sass    = require('gulp-sass');
-var concat  = require('gulp-concat');
-var uglify  = require('gulp-uglify');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+var css = require('gulp-css');
+var minify = require('gulp-minify');
+var watch = require('gulp-watch');
+var batch = require('gulp-batch');
+
 
 var dir = {
     assets: './src/AppBundle/Resources/',
     dist: './web/',
-    npm: './node_modules/',
+    npm: './node_modules/'
 };
 
-gulp.task('sass', function() {
+
+gulp.task('sass', function () {
     gulp.src(dir.assets + 'style/main.scss')
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(concat('style.css'))
         .pipe(gulp.dest(dir.dist + 'css'));
 });
 
-gulp.task('scripts', function() {
-    gulp.src([
-            //Third party assets
-            dir.npm + 'jquery/dist/jquery.min.js',
-            dir.npm + 'bootstrap-sass/assets/javascripts/bootstrap.min.js',
 
-            // Main JS file
-            dir.assets + 'scripts/main.js'
-        ])
-        .pipe(concat('script.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(dir.dist + 'js'));
-});
-
-gulp.task('images', function() {
+gulp.task('images', function () {
     gulp.src([
-            dir.assets + 'images/**'
-        ])
+        dir.assets + 'images/**'
+    ])
         .pipe(gulp.dest(dir.dist + 'images'));
 });
 
-gulp.task('fonts', function() {
+gulp.task('fonts', function () {
     gulp.src([
         dir.npm + 'bootstrap-sass/assets/fonts/**'
-        ])
+    ])
         .pipe(gulp.dest(dir.dist + 'fonts'));
 });
 
-gulp.task('default', ['sass', 'scripts', 'fonts', 'images']);
+gulp.task('js', function () {
+    gulp.src([
+        dir.npm + 'jquery/dist/jquery.min.js',
+        dir.npm + 'bootstrap-sass/assets/javascripts/bootstrap.min.js',
+    ])
+        .pipe(concat('main.js'))
+        .pipe(minify())
+        .pipe(gulp.dest(dir.dist + 'js'))
+});
+
+gulp.task('admin-js', function () {
+    gulp.src([
+        dir.npm + 'jquery/dist/jquery.min.js',
+    ])
+        .pipe(concat('admin.js'))
+        .pipe(minify())
+        .pipe(gulp.dest(dir.dist + 'js'))
+});
+
+gulp.task('default', ['sass', 'js', 'admin-js', 'fonts', 'images']);
