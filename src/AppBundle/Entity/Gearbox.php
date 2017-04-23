b17212cc9539e65051c6614f537570056c9ad7a5
Tomas Nemeikšis <niumis@gmail.com>
2017-04-23 17:23:43 +0300

    Create Gearbox entity

---
+++ src/AppBundle/Entity/Gearbox.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Gearbox
+ *
+ * @ORM\Table(name="gearbox")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\GearboxRepository")
+ */
+class Gearbox
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Gearbox
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
+
