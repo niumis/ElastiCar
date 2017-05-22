c9f83aa09296fe9ffd4ad68f58b47aa3c0570fee
Tomas Nemeikšis <niumis@gmail.com>
2017-05-22 22:25:35 +0300

    Create registration

---
+++ src/AppBundle/Controller/Web/RegistrationController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niumis
+ * Date: 2017-05-22
+ * Time: 21:36
+ */
+
+namespace AppBundle\Controller\Web;
+
+use AppBundle\Form\UserType;
+use AppBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class RegistrationController extends Controller
+{
+    /**
+     * @Route("/register", name="user_registration")
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+        }
+
+        return $this->render(
+            'AppBundle:Security:register.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+}
